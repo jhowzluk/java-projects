da6643c9224d29d7992fdfa96d42489d4b3b2bf8
jhowzluk <jhonatanluksik@gmail.com>
2026-03-18 11:35:11 -0300

    feat: diretório para estudos de POO

---
+++ POO/src/encapsulamento/application/Program.java
@@ -0,0 +1,18 @@
+package encapsulamento.application;
+
+import encapsulamento.entidades.Conta;
+
+public class Program {
+    public static void main(String[] args) {
+        Conta c = new Conta();
+
+        c.nome = "Jhow";
+        c.cpf = "117282222";
+        c.depositar(100.0);
+        c.sacar(20.0);
+
+        System.out.println(c.nome);
+        System.out.println(c.cpf);
+        System.out.println(c.getSaldo());
+    }
+}
